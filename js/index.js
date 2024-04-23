2ad686b6918bb0364b37b12a67f93bd6e8c59654
HiYUNo <jayhyun1008@gmail.com>
2024-04-24 07:16:34 +0900

    still fixing image deleting...

--- js/index.js
+++ js/index.js
@@ -840,7 +840,7 @@ async function parseYourJSON(json) {
             document.querySelector('.editform').innerHTML += '<textarea id="cContent" name="cContent"></textarea>'
 
             //파일첨부
-            document.querySelector('.editform').innerHTML += '<div class="editordiv" id="imgUploader"><div><span class="bold">'+LANG.ADDFILE+'</span> <span id="imgUpload">'+LANG.CLICK+'</span></div></div><input type="file" id="imgRealUpload" accept="image/*" style="display: none;">'
+            document.querySelector('.editform').innerHTML += '<div class="editordiv" id="imgUploader"><div id="imageUploadFrame0"><span class="bold">'+LANG.ADDFILE+'</span> <span id="imgUpload">'+LANG.CLICK+'</span></div></div><input type="file" id="imgRealUpload" accept="image/*" style="display: none;">'
 
             //확인 버튼
             document.querySelector('.editform').innerHTML += '<div class="editordiv"><span class="bold" id="confirm">'+LANG.CONFIRM+'</span> <span class="bold" id="cancel">취소</span>'
@@ -889,12 +889,12 @@ async function parseYourJSON(json) {
                         document.querySelector('#imgUpload').innerText = imgRes.id
                         document.querySelector('#imgUpload').classList.add('imgUploaded')
                         document.querySelector('#imgUpload').id = 'imgUploaded'+fileCount
-
-                        document.querySelector('#imgUploader').innerHTML += '<div><span class="bold">'+LANG.ADDFILE+'</span> <span id="imgUpload">'+LANG.CLICK+'</span></div>'
                         fileCount += 1
 
+                        document.querySelector('#imgUploader').innerHTML += '<div id="imgUploadFrame'+fileCount+'"><span class="bold">'+LANG.ADDFILE+'</span> <span id="imgUpload">'+LANG.CLICK+'</span></div>'
+
                         for (var i=0; i<fileCount; i++) {
-                            document.querySelector('#imgUploaded'+i).addEventListener("click", (e) => {
+                            document.querySelector('#imgUploadFrame'+i).addEventListener("click", (e) => {
                                 var isDeleting = confirm(LANG.cDELETEFILE)
                                 if (isDeleting) {
                                     e.currentTarget.remove()
@@ -2119,11 +2119,18 @@ async function parseYourJSON(json) {
                 document.querySelector('.editform').innerHTML += '<div class="editordiv" id="imgUploader"></div><input type="file" id="imgRealUpload" accept="image/*" style="display: none;">'
                 
                 for (var i=0; i<notesRes.fileIds.length; i++) {
-                    document.querySelector('#imgUploader').innerHTML += '<div><span class="bold">'+LANG.ADDFILE+'</span> <span class="imgUploaded" id="imgUploaded'+i+'">'+notesRes.fileIds[i]+'</span></div>'
+                    document.querySelector('#imgUploader').innerHTML += '<div id="imgUploadFrame'+i+'"><span class="bold">'+LANG.ADDFILE+'</span> <span class="imgUploaded" id="imgUploaded'+i+'">'+notesRes.fileIds[i]+'</span></div>'
+
+                    document.querySelector('#imgUploadFrame'+i).addEventListener("click", (e) => {
+                        var isDeleting = confirm(LANG.cDELETEFILE)
+                        if (isDeleting) {
+                            e.currentTarget.remove()
+                        }
+                    })
                 }
                 fileCount = notesRes.fileIds.length
 
-                document.querySelector('#imgUploader').innerHTML += '<div><span class="bold">'+LANG.ADDFILE+'</span> <span id="imgUpload">'+LANG.CLICK+'</span></div>'
+                document.querySelector('#imgUploader').innerHTML += '<div id="imgUploadFrame'+fileCount+'"><span class="bold">'+LANG.ADDFILE+'</span> <span id="imgUpload">'+LANG.CLICK+'</span></div>'
     
                 //확인 버튼
                 document.querySelector('.editform').innerHTML += '<div class="editordiv"><span class="bold" id="confirm">'+LANG.CONFIRM+'</span> <span class="bold" id="cancel">취소</span>'
@@ -2171,12 +2178,12 @@ async function parseYourJSON(json) {
                             document.querySelector('#imgUpload').innerText = imgRes.id
                             document.querySelector('#imgUpload').classList.add('imgUploaded')
                             document.querySelector('#imgUpload').id = 'imgUploaded'+fileCount
-
-                            document.querySelector('#imgUploader').innerHTML += '<div><span class="bold">'+LANG.ADDFILE+'</span> <span id="imgUpload">'+LANG.CLICK+'</span></div>'
                             fileCount += 1
 
+                            document.querySelector('#imgUploader').innerHTML += '<div id="imgUploadFrame'+fileCount+'"><span class="bold">'+LANG.ADDFILE+'</span> <span id="imgUpload">'+LANG.CLICK+'</span></div>'
+
                             for (var i=0; i<fileCount; i++) {
-                                document.querySelector('#imgUploaded'+i).addEventListener("click", (e) => {
+                                document.querySelector('#imgUploadFrame'+i).addEventListener("click", (e) => {
                                     var isDeleting = confirm(LANG.cDELETEFILE)
                                     if (isDeleting) {
                                         e.currentTarget.remove()
@@ -2212,12 +2219,6 @@ async function parseYourJSON(json) {
                     var cFile = []
                     for (var i=0; i < Math.min(document.querySelectorAll('.imgUploaded').length, 16); i++) {
                         cFile.push(document.querySelector('#imgUploaded'+i).innerText)
-                        document.querySelector('#imgUploaded'+i).addEventListener("click", (e) => {
-                            var isDeleting = confirm(LANG.cDELETEFILE)
-                            if (isDeleting) {
-                                e.currentTarget.remove()
-                            }
-                        })
                     }
                     
                     var createNoteUrl = 'https://'+MISSKEYHOST+'/api/notes/create'
